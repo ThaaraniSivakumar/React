13993f77bdfedfeac098aab8d8f173cb72a3661c
ThaaraniShivakumar <sivathaarani2004@gmail.com>
2025-07-31 21:42:36 +0530

    installed yup and hookform/resolvers

---
+++ practice/src/components/ConfirmPassword.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ConfirmPassword = () => {
+  return (
+    <div>
+      <form>
+        <input ></input>
+        <input></input>
+        <input></input>
+        <input></input>
+      </form>
+    </div>
+  )
+}
+
+export default ConfirmPassword
