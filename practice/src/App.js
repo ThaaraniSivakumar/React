2b2e8e7804287b08f5910b0a3464c17c06a02d6d
ThaaraniShivakumar <sivathaarani2004@gmail.com>
2025-08-01 07:54:28 +0530

    created contact form and it worked perfectly

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import ConfirmPassword from './components/ConfirmPassword';
+import ContactForm from './components/ContactForm';
 import LoginForm from './components/LoginForm';
 
 function App() {
   return (
     <div>
       {/* <LoginForm/> */}
-      <ConfirmPassword/>
+      {/* <ConfirmPassword/> */}
+      <ContactForm/>
     </div>
   );
 }
--- practice/src/components/ContactForm.js
+++ practice/src/components/ContactForm.js
@@ -8,9 +8,9 @@ const ContactForm = () => {
         name:yup.string().required("Enter name"),
         email:yup.string().required("Enter Email").email("Invalid format"),
         phoneNumber:yup.string().required("Enter Phone Number").max(10,"Max should be 10 numbers"),
-        message:yup.string().required("Enter any message")
+        Message:yup.string().required("Enter any message")
     })
-    const{register,handleSubmit,formState:{errors}}=useForm()
+    const{register,handleSubmit,formState:{errors}}=useForm({resolver:yupResolver(schema)})
     const onSubmit=(data)=>{
         console.log(data)
         alert("Contact Form Sumitted")
@@ -18,14 +18,14 @@ const ContactForm = () => {
   return (
     <div>
         <form onSubmit={handleSubmit(onSubmit)}>
-      <inputt type="text" {...register("name")} ></inputt>
+      <input type="text" {...register("name")} ></input><br></br>
       {errors.name && <p>{errors.name.message}</p>}
-      <iput type="email" {...register("email")}></iput>
-            {errors.name && <p>{errors.name.message}</p>}
-      <input {...register("phoneNumber")}></input>
-            {errors.name && <p>{errors.name.message}</p>}
-      <input type="text" {...register("message")}></input>
-            {errors.name && <p>{errors.name.message}</p>}
+      <input type="email" {...register("email")}></input><br></br>
+            {errors.email && <p>{errors.email.message}</p>}
+      <input {...register("phoneNumber")}></input><br></br>
+            {errors.phoneNumber && <p>{errors.phoneNumber.message}</p>}
+      <input type="text" {...register("Message")}></input><br></br>
+            {errors.Message && <p>{errors.Message.message}</p>}
      <button type='submit'>Submit</button>
       </form>
     </div>
